625ac323bc289b598cad340fadbdfa98cd863e79
A7ES <125215896+A7ES@users.noreply.github.com>
2023-04-01 15:27:32 +0100

    Update chat_gpt.py
    Can now extract the product information when given an array of product names.

--- app/chat_gpt.py
+++ app/chat_gpt.py
@@ -24,12 +24,43 @@ def extractInfoOne(productTitle):
 
 # When given an array of product names, returns the product names and manufacturers as an array. May return None.
 def extractInfoMultiple(productNames):
-    pass
+    productNames = str(productNames)
+    try:
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                                messages=[{"role": "system",
+                                                           "content": "From now on, I will give you a list of product titles. "
+                                                                      "For each, return the product name and the manufacturer, "
+                                                                      "split by a comma."},
+                                                          {"role": "user",
+                                                          "content": productNames}])
+
+        print(response)
+        return response.choices[0].message.content.split("\n")
+
+    except Exception as e:
+        print(e)
+        return None
 
 
 # When given a product name, returns an array of the estimates of the carbon footprint in kgs(?). May
 # return None.
-def getCarbonInfo(productName):
+def getCarbonInfoOne(productName):
+    try:
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                                messages=[{"role": "system",
+                                                           "content": "From now on, I will give you a product title. "
+                                                                      "Return the carbon footprint in kgs with no other words."},
+                                                          {
+                                                              "role": "user",
+                                                              "content": productName}])
+        return response.choices[0].message.content
+    except Exception as e:
+        return None
+
+
+# When given a product name, returns an array of the estimates of the carbon footprint in kgs(?). May
+# return None.
+def getCarbonInfoMultiple(productNames):
     try:
         response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
                                                 messages=[{"role": "system",
@@ -43,4 +74,6 @@ def getCarbonInfo(productName):
         return None
 
 
-print(getCarbonInfo("Apple iPhone 13"))
+
+arrayOfProductNames = ("Apple iPhone 13", "Lenovo pc", "primark t-shirt")
+print(extractInfoMultiple(arrayOfProductNames))
