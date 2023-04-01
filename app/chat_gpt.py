bed1eba00ae84cea74b862c4415b0951f525a9e0
A7ES <125215896+A7ES@users.noreply.github.com>
2023-04-01 14:35:54 +0100

    chat_gpt
    extractInfoOne(productTitle) created

---
+++ app/chat_gpt.py
@@ -0,0 +1,33 @@
+import openai
+
+# Put key in env file.
+openai.api_key = "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"
+
+
+# When given a product title, returns the product name and manufacturer. May return None.
+def extractInfoOne(productTitle):
+    try:
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                                messages=[{"role": "system",
+                                                           "content": "From now on, I will give you a product title. "
+                                                                      "Return the product name and the manufacturer, "
+                                                                      "split by a comma."},
+                                                          {
+                                                              "role": "user",
+                                                              "content": productTitle}])
+        response = response.split(",")
+        return response
+    except Exception as e:
+        return None
+
+
+# When given an array of product titles, returns the product names and manufacturers as an array. May return None.
+def extractInfoMultiple(productTitles):
+    pass
+
+
+# When given an array of product titles, returns an array of the estimates of the carbon footprint in kgs(?). May
+# return None.
+def getCarbonInfo(productTitles):
+    pass
+
