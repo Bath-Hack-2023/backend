6161ca7f8efcc1c1794883689b30aa89ca36a1a2
Varniethan Ketheeswaran <varniethan@protonmail.com>
2023-04-01 14:19:18 +0100

    ditch carbon api works

---
+++ app/ditch_carbon.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+def getCarbonData(product, manufacturer):
+    url = f"https://api.ditchcarbon.com/v1.0/product?name={product}&manufacturer={manufacturer}"
+    headers = {"accept": "application/json", "Authorization": "Bearer 92d3af4ac158430ec82fd01adf0093de"}
+    response = (requests.get(url, headers=headers)).json()
+    if "errors" in response:
+        #TODO: Handle error with chatgpt
+        print("This is an error")
+    else:
+        return response["carbon_footprint"]
+
+getCarbonData("Cat food", "Mars Incorporated")
